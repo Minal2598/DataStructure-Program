ebef47e96385f618636688b129ade22834a438a4
Minal <patilminalp@gmail.com>
2021-07-01 00:48:33 +0530

    Added:Welcome to dataStructure Program

---
+++ src/com/datastructure/Main.java
@@ -0,0 +1,21 @@
+package com.datastructure;
+
+public class Main {
+
+
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to DataStructure Program");
+	
+		LinkedList linkedList = new LinkedList();
+		linkedList.insert(56);
+		linkedList.insert(30);
+		linkedList.insert(70);
+		
+		linkedList.print();
+		
+		
+		
+	}
+
+}
