f35cddfd5145d8e5e721ba02518198ca7122b84e
Minal <patilminalp@gmail.com>
2021-07-03 19:02:15 +0530

    Modified:List modify by deleting element from tail

--- src/com/datastructure/LinkedList.java
+++ src/com/datastructure/LinkedList.java
@@ -1,60 +1,58 @@
 package com.datastructure;
 
 public class LinkedList {
-	//head of list
-	Node head;	 
-	
-	class Node{
+	// head of list
+	Node head;
+
+	class Node {
 		int data;
 		Node next;
-		
+
 		public Node(int data) {
 			this.data = data;
-			this.next = null; //by default object value is null
+			this.next = null; // by default object value is null
 		}
 	}
-	
+
 	// This is inert (Append) method
 	public Node insert(int data) {
 		Node newNode = new Node(data);
-		if(head == null) {
+		if (head == null) {
 			head = newNode;
-		}
-		else {
+		} else {
 			Node temp = head;
-			while(temp.next != null) {
+			while (temp.next != null) {
 				temp = temp.next;
 			}
 			temp.next = newNode;
 		}
 		return newNode;
 	}
-	
+
 	public void push(int data) {
 		Node newNode = new Node(data);
-		newNode .next = head;
+		newNode.next = head;
 		head = newNode;
-			
-		
+
 	}
-       
+
 	// Additional data put in betn previous data
-	
+
 	public void insertAfter(Node prevNode, int value) {
 		if (prevNode == null) {
 			System.out.println("Prev Node should not be null");
 			return;
-			}
-			
-			Node newNode = new Node(value);
-			newNode.next = prevNode.next;
-			prevNode.next = newNode;
+		}
 
-			print();
-		
+		Node newNode = new Node(value);
+		newNode.next = prevNode.next;
+		prevNode.next = newNode;
+
+		print();
+
 	}
-	
-	//Delete first element
+
+	// Delete first element
 	public int pop() {
 		int popData = 0;
 		if (head == null) {
@@ -62,37 +60,36 @@ public class LinkedList {
 		}
 		popData = head.data;
 		head = head.next;
-		
+
 		return popData;
 	}
-	
+
+	// Delete last element using tail
+	public void popLast() {
+		if (head == null) {
+			System.out.println("Stack is empty");
+		}
+		if (head == null) {
+			System.out.println(head.data);
+			head = null;
+		}
+		Node temp = head;
+		while (temp.next.next != null) {
+			temp = temp.next;
+
+		}
+		temp.next = null;
+		System.out.println("Delete last node::");
+
+	}
 
 	// print the output
 	public void print() {
 		Node temp = head;
-		while(temp != null) {
-			System.out.print(temp.data +  "-> ");
+		while (temp != null) {
+			System.out.print(temp.data + "-> ");
 			temp = temp.next;
 		}
+	}
+
 }
-	public int search(int value) {
-		if (head == null) {
-		System.out.println("List is Empty");
-		return -1;
-		}
-		int index = 1;
-		Node tempNode = head;
-		while (tempNode != null) {
-		if (tempNode.data == value) {
-		break;
-		}
-		index++;
-		tempNode = tempNode.next;
-		}
-		return index;
-		}
-	
-		
-	
-}
-	
--- src/com/datastructure/Main.java
+++ src/com/datastructure/Main.java
@@ -1,28 +1,16 @@
 package com.datastructure;
 
-import com.datastructure.LinkedList.Node;
-
 public class Main {
-  public static void main(String[] args) {
+	public static void main(String[] args) {
 		System.out.println("Welcome to DataStructure Program");
 		LinkedList linkedList = new LinkedList();
-	
+
 		linkedList.insert(56);
-    	linkedList.insert(30);
-    	linkedList.insert(70);
-   
-    	
-    	/*int pop = linkedList.lastPop();
-    	System.out.println(pop);*/
-    //	linkedList.print();
-    	System.out.println("\n");
-    	int search = linkedList.search(30);
-    	System.out.println("Position of 30::"+search);   	
-    	
-    	
-    	
-    	
-    	
-    	
+		linkedList.insert(30);
+		linkedList.insert(70);
+		linkedList.popLast();
+
+		linkedList.print();
+
 	}
 }
